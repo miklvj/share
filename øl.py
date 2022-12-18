3bae78efb50bb47f6b28e8aa255a42fe737c6c7a
miklvj <120057518+miklvj@users.noreply.github.com>
2022-12-18 10:56:15 +0100

    Update øl.py

--- øl.py
+++ øl.py
@@ -28,37 +28,37 @@ PLUG IN STATS
 
 
 
-navn = ['Punk IPA', 'Aarhus Tribute', 'Santa Gose', 'Red Noses', 'Christmas Ale', 'Yule Juice', 'Yuzual Suspect', 'Hazy Jane', 'No.5 Valnød', 'Snug as a Bug in a Rug', 'Santas Hoppy Helpers', 'London Lager', 'Luxus Jule Bryg', 'Saison', 'Kilo the King og Cuddles', 'Duvel']
-bryggeri = ['Brewdog', 'Aarhus Bryghus', 'To Øl', 'Anarkist', 'Jacobsen', 'Amager Bryghus', 'To Øl', 'Brewdog', 'Ærø Bryghus', 'Salikatt', 'Mikkeller', 'Poppels', 'Godt Bryg', 'Ebletoft Gårdbryggeri', 'Amager Bryghus', 'Duvel']
-type = ['IPA', 'Barleywine', 'Sour', 'Red Ale', 'Red Ale', 'IPA', 'Sour', 'IPA', 'Bock', 'NEIPA', 'IPA', 'Lager', 'Pilsner', 'Saison', 'IPA', 'Blond']
-procent = [5.4, 9.0, 4.0, 5.5, 7.5, 6.5, 4.5, 5.0, 7.0, 8.0, 6.0, 5.0, 7.0, 8.1, 5.8, 8.5]
+navn = ['Punk IPA', 'Aarhus Tribute', 'Santa Gose', 'Red Noses', 'Christmas Ale', 'Yule Juice', 'Yuzual Suspect', 'Hazy Jane', 'No.5 Valnød', 'Snug as a Bug in a Rug', 'Santas Hoppy Helpers', 'London Lager', 'Luxus Jule Bryg', 'Saison', 'Kilo the King og Cuddles', 'Duvel', 'Basic Shake', 'Chitfaced']
+bryggeri = ['Brewdog', 'Aarhus Bryghus', 'To Øl', 'Anarkist', 'Jacobsen', 'Amager Bryghus', 'To Øl', 'Brewdog', 'Ærø Bryghus', 'Salikatt', 'Mikkeller', 'Poppels', 'Godt Bryg', 'Ebletoft Gårdbryggeri', 'Amager Bryghus', 'Duvel', 'Brewdog', 'To Øl']
+type = ['IPA', 'Barleywine', 'Sour', 'Red Ale', 'Red Ale', 'IPA', 'Sour', 'IPA', 'Bock', 'NEIPA', 'IPA', 'Lager', 'Pilsner', 'Saison', 'IPA', 'Blond', 'IPA', 'IPA']
+procent = [5.4, 9.0, 4.0, 5.5, 7.5, 6.5, 4.5, 5.0, 7.0, 8.0, 6.0, 5.0, 7.0, 8.1, 5.8, 8.5, 4.7, 7.5]
 
 
 '''
 Ems
 '''
 
-duft_ems = [4, 4, 5, 2, 4, 5, 1, 3, 2, 4, 3, 3, 1, 5, 5, 3]
-smag_ems = [8, 6, 9, 4, 7, 6, 6, 6, 5, 8, 7, 6, 4, 8, 8, 8]
-helhedsoplevelse_ems = [4, 3, 5, 2, 3, 4, 4, 3, 2, 4, 4, 3, 2, 4, 4, 4]
+duft_ems = [4, 4, 5, 2, 4, 5, 1, 3, 2, 4, 3, 3, 1, 5, 5, 3, 4, 'Na']
+smag_ems = [8, 6, 9, 4, 7, 6, 6, 6, 5, 8, 7, 6, 4, 8, 8, 8, 8, 'Na']
+helhedsoplevelse_ems = [4, 3, 5, 2, 3, 4, 4, 3, 2, 4, 4, 3, 2, 4, 4, 4, 4, 'Na']
 
 
 '''
 Tejl
 '''
 
-duft_tejl = [5, 2, 4, 2, 4, 4, 1, 2, 4, 5, 5, 3, 2, 5, 4, 2]
-smag_tejl = [7, 9, 7, 5, 7, 6, 8, 6, 6, 9, 8, 5, 4, 8, 4, 8]
-helhedsoplevelse_tejl = [4, 3, 4, 3, 4, 2, 4, 3, 3, 5, 5, 3, 2, 4, 3, 3]
+duft_tejl = [5, 2, 4, 2, 4, 4, 1, 2, 4, 5, 5, 3, 2, 5, 4, 2, 4, 'Na']
+smag_tejl = [7, 9, 7, 5, 7, 6, 8, 6, 6, 9, 8, 5, 4, 8, 4, 8, 6, 'Na']
+helhedsoplevelse_tejl = [4, 3, 4, 3, 4, 2, 4, 3, 3, 5, 5, 3, 2, 4, 3, 3, 4, 'Na']
 
 
 '''
 Miks
 '''
 
-duft_miks = [3, 4, 4, 2, 3, 5, 3, 2, 3, 4, 3, 4, 2, 5, 5, 2]
-smag_miks = [7, 6, 9, 4, 6, 7, 9, 5, 4, 7, 7, 7, 4, 9, 6, 3]
-helhedsoplevelse_miks = [3, 3, 5, 2, 3, 3, 5, 3, 2, 4, 4, 4, 2, 5, 3, 2]
+duft_miks = [3, 4, 4, 2, 3, 5, 3, 2, 3, 4, 3, 4, 2, 5, 5, 2, 4, 4]
+smag_miks = [7, 6, 9, 4, 6, 7, 9, 5, 4, 7, 7, 7, 4, 9, 6, 3, 8, 9]
+helhedsoplevelse_miks = [3, 3, 5, 2, 3, 3, 5, 3, 2, 4, 4, 4, 2, 5, 3, 2, 4, 5]
 
 
 
